83a6991cbc84a88e1b23c956d82fdfaddd1468cd
SKX Kafei <josue.ham@unitec.edu>
2023-04-28 15:39:42 -0600

    Lab terminado

--- Lab2P3_Josue_Ham.cpp
+++ Lab2P3_Josue_Ham.cpp
@@ -7,16 +7,47 @@ int random() {
 	int aleatorio = 97 + rand() % (123 - 97);
 	return aleatorio;
 }
-void arreglo_Inicial() {
+void arreglos() {
 	int array[10];
 	cout << "Arreglo inicial: ";
 	for (int i = 0; i < 10; i++)
 	{
 		array[i] = random();
 		cout << "[" << (char)array[i] << "]";
-	}
+	} //Este for rellena el arreglo inicial de chars aleatorios
 
+	int arregloFinal[10];
+	int auxiliar;
+	int contador = 0;
+	int valorAnterior; //Variable para salvar el valor anterior
 
+	for (int k = 0; k < 10; k++)
+	{
+		if (array[k] != array[k+1])
+		{
+			arregloFinal[k] = array[k];
+		}
+	}//Le asigno los valores del arreglo inicial al arreglo a ordenar
+
+	printf("\n");
+	cout << "Nuevo arreglo con caracteres ordenados y sin repetir: ";
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (arregloFinal[j] > arregloFinal[j+1])
+			{
+				auxiliar = arregloFinal[j];
+				arregloFinal[j] = arregloFinal[j + 1];
+				arregloFinal[j + 1] = auxiliar;
+			}
+		}
+	}//Metodo de burbuja para ordenar
+
+	for (int l = 0; l < 10; l++)
+	{
+		cout << "[" << (char)arregloFinal[l] << "]";
+	}//Solo imprime
 }
 //Metodos ejercicio 2
 int Factorial(int numero) {
@@ -64,7 +95,7 @@ int main()
 	while (respuesta !=3){
 		switch (respuesta) {
 		case 1: {	
-			arreglo_Inicial();
+			arreglos();
 
 		}
 			  break;
